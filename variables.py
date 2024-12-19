affa397fd7935e7fd85629a597c363a124b6a2e9
Leefinix <joaquinnleo@gmail.com>
2024-12-19 19:02:53 +0700

    variables

---
+++ variables.py
@@ -0,0 +1,11 @@
+x = 5/9
+y = 2/9
+print(x)
+print(y)
+print("The sum of the 2 numbers are 7/9")
+
+math = "math"
+print(math)
+
+name = input("Enter your name: ")
+print("Hello!", name,"Welcome to Math School!")
